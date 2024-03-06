445fb3d9b9bfae2bb7cbe92173430c26129b41c2
adna olívia <88201392+adnaolivia@users.noreply.github.com>
2024-03-05 21:12:48 -0300

    script

---
+++ script.js
@@ -0,0 +1,13 @@
+//Q1 - Insere texto no H1
+
+
+//Q2 - Insere atributo placeholder nas tags input
+
+
+//Q3 - Trocar a cor de background do botão de confirmação usando a propriedade style
+
+
+//Q4 - tocar cor das tags label usando classList
+
+
+//Q5 - adicionar uma tag de hiperlink <a> abaixo dos botões do fomulário
